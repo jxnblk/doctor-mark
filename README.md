5c59deca263dd9584fcc94cb34654895cec6d455
Brent Jackson <jxnblk@github.com>
2015-03-16 14:44:47 -0400

    Basic setup

--- README.md
+++ README.md
@@ -2,30 +2,3 @@
 
 An Automatic Page Generation Tool
 
-*Experimental Work in Progress*
-
-Use at your own risk
-
----
-
-
-## Highlighted Code Examples
-
-By using a bit of markup in your README.md, you can include live HTML code examples.
-(Take a look at the README.md file on Github to see how it works)
-
-<div data-include="docs/examples/headings.html"></div>
-
-<div data-include-example="docs/examples/headings.html"></div>
-
----
-
-# Options Notes
-- `css` - file path to inline styles
-- `cssURL` - url for stylesheet link tag
-
-# To Do
-- Styles for DM-example component
-- Style for TOC
-- Add themes option
-
--- index.js
+++ index.js
@@ -2,52 +2,23 @@
   
    Doctor Mark
 
-   *WIP*
    For converting Markdown strings to simple HTML
    pages for documentation
 
 */
 
+var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
-var through = require('through2');
 var marked = require('marked');
-var cheerio = require('cheerio');
+var markedExample = require('marked-example');
 
-var include = require('./lib/include-html');
-var example = require('./lib/include-html-example');
-var css = require('./lib/include-css');
-var header = fs.readFileSync(__dirname + '/lib/header.html', 'utf8');
-var footer = fs.readFileSync(__dirname + '/lib/footer.html', 'utf8');
+var renderer = new marked.Renderer();
 
-marked.setOptions(require('./lib/marked-options'));
+module.exports = function(md, options) {
 
-module.exports = function(options) {
-
-  var options = options || {};
-
-  return through.obj(function(file, enc, callback) {
-
-    var self = this;
-    var string = file.contents.toString();
-
-    var html = marked(string);
-    html = header + '\n' + html + '\n' + footer;
-    html = include(html);
-    html = css(html, options);
-
-    var $ = cheerio.load(html);
-    var title = $('h1').first().text();
-    $('title').html(title);
-    
-    example($.html(), function(result) {
-      file.contents = new Buffer(result);
-      var filename =  path.basename(file.path, path.extname(file.path)) + '.html';
-      file.path = path.join(path.dirname(file.path), filename);
-      self.push(file);
-      callback();
-    });
-
+  var options = _.defaults(options, {
+    template: fs.readFileSync('', 'utf8'),
   });
 
 };
